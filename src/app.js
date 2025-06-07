28a11d68b61c3f17c4f31abc2846f39e0f965d6a
Samuel Amponsah Asiedu <amponsahsam932@gmail.com>
2025-06-07 20:11:35 +0000

    exploring GET, POST, DELETE

--- src/app.js
+++ src/app.js
@@ -2,13 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req, res) => {
-  res.send("Hello from server 3000");
+// app.use("/",(req, res) => {
+//   res.send("Hello from server 3000");
+// });
+app.get("/user", (req,res) => {
+    res.send({firstName: "Samuel", lastName: "Asiedu"})
+})
+
+app.post("/user", (req,res) =>{
+    console.log("save data to database");
+    res.send("data saved to database");
 });
+
+app.delete("/user", (req,res) =>{
+    res.send("deleted succesfully")
+})
 
 app.use("/hello", (req, res) => {
   res.send("Hello Hello Hello");
 });
+
+app.use("/test",(req, res) => {
+    res.send("Hello from the server")
+})
 
 app.listen(3000, () => {
   console.log("server is running on prt 3000");
