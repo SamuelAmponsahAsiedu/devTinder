9d40417f8b9cf23830a43826234c3f39b20678c6
Samuel Amponsah Asiedu <asiedusam932@gmail.com>
2025-06-15 17:46:58 +0000

    handling-multiple-routes

--- src/app.js
+++ src/app.js
@@ -2,30 +2,24 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/",(req, res) => {
-//   res.send("Hello from server 3000");
-// });
-app.get("/user", (req,res) => {
-    res.send({firstName: "Samuel", lastName: "Asiedu"})
-})
-
-app.post("/user", (req,res) =>{
-    console.log("save data to database");
-    res.send("data saved to database");
-});
-
-app.delete("/user", (req,res) =>{
-    res.send("deleted succesfully")
-})
-
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Hello");
-});
-
-app.use("/test",(req, res) => {
-    res.send("Hello from the server")
-})
-
+app.use(
+    "/user",
+    (req, res, next) => {
+        console.log("handling the route user");
+        //res.send("1st Response");
+        next();
+    },
+    (req, res, next) =>{
+        console.log("handling the route handler 2");
+        //res.send("2nd Respoonse");
+        next()
+    },
+    (req,res,next) => {
+        console.log("handling the route user 2");
+        res.send("3rd Response");
+        //next()
+    }
+)
 app.listen(3000, () => {
   console.log("server is running on prt 3000");
 });
